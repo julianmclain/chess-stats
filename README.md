23dc9df2a63e250da3d7b743cc46b74205c13b4a
julianmclain <julianrmclain@gmail.com>
2020-08-28 22:01:56 -0700

    Add title to graph

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Chess Stats
 
 TODO
+- docstrings
 - beef up how requests are made (e.g. timeouts)
-- chart wins and losses by month
 - test
 - pypi
+- chart wins and losses by month
 
 usage: chess-stats jjjulio 2020
--- src/chess_stats/chess_stats.py
+++ src/chess_stats/chess_stats.py
@@ -18,6 +18,7 @@ def compute_annual_summary(username: str, year: int) -> AnnualSummary:
 
     return AnnualSummary(year, wins, losses, draws)
 
+
 def graph_annual_summary(summary: AnnualSummary) -> None:
     """
 
@@ -30,11 +31,14 @@ def graph_annual_summary(summary: AnnualSummary) -> None:
         "Draws": summary.draws,
     }
     max_label_length = max((len(label) for label in data.keys()))
+    max_value_length = max((len((str(value))) for value in data.keys()))
     increment = 200 / max(data.values())
+    title = f"\nYour {summary.year} Chess.com Year in Review\n"
+    print(title)
     for label, value in data.items():
         num_bars, remainder = divmod(int(value * 8 / increment), 8)
         bar = "█" * num_bars
         remainder_bar = chr(ord("█") + remainder)
         bar += remainder_bar
         empty = "▏"
-        print(f"{label.rjust(max_label_length)} | {value} {bar}")
+        print(f"{label.rjust(max_label_length)} | {str(value).rjust(max_value_length)} {bar}")
